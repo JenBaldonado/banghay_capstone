568383b2dcc55cce5d864b624699b827fe245af9
Jenza <spotty0326@gmail.com>
2022-12-09 23:30:37 +0800

    presentation

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,18 @@ use App\Http\Controllers\BanghayController;
 Route::get('/', [BanghayController::class, 'welcome']);
 Route::get('/banghay', [BanghayController::class, 'index'])->middleware('auth');
 Route::post('/uploadfile', [BanghayController::class, 'store'])->middleware('auth');
-
-
 Route::get('/download/{file}', [BanghayController::class, 'download']);
 Route::get('/preview/{id}', [BanghayController::class, 'preview']);
+
 Route::get('/banghay/gradeone', [BanghayController::class, 'gradeone'])->middleware('auth');
 Route::get('/banghay/gradetwo', [BanghayController::class, 'gradetwo'])->middleware('auth');
 Route::get('/banghay/gradethree', [BanghayController::class, 'gradethree'])->middleware('auth');
 Route::get('/banghay/gradefour', [BanghayController::class, 'gradefour'])->middleware('auth');
 Route::get('/banghay/gradefive', [BanghayController::class, 'gradefive'])->middleware('auth');
 Route::get('/banghay/gradesix', [BanghayController::class, 'gradesix'])->middleware('auth');
+
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
