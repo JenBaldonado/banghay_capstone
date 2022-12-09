229f19ae5cc176e0aa9bd644900bc63d40713db8
Jenza <spotty0326@gmail.com>
2022-12-09 23:28:40 +0800

    presentation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -223,8 +223,8 @@
             </div>
           </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-    
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+ -->    
 
 
 </body>
--- resources/views/layouts/glvlLayout.blade.php
+++ resources/views/layouts/glvlLayout.blade.php
@@ -219,8 +219,8 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
-
+<!--     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+ -->
     <script>
         const body = document.querySelector('body'),
             sidebar = body.querySelector('nav'),
