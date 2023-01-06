78a11e2b1fabe2eb45f246def3f9f39656b062c1
Jenza <spotty0326@gmail.com>
2023-01-06 20:09:25 +0800

    clean up a little

--- resources/views/layouts/glvlLayout.blade.php
+++ resources/views/layouts/glvlLayout.blade.php
@@ -221,8 +221,11 @@
  -->
     <script>
         const body = document.querySelector('body'),
+            sidebar = body.querySelector('nav'),
             toggle = body.querySelector(".toggle"),
+            modeSwitch = body.querySelector(".toggle-switch"),
             modeText = body.querySelector(".mode-text");
+
 
         toggle.addEventListener("click", () => {
             sidebar.classList.toggle("close");
